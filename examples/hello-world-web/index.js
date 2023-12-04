4ee41606e0dd7c022df983ad0d7e6984882cf439
Purvi Kanal <purvikanal@honeycomb.io>
2023-12-04 13:58:48 -0500

    feat:  ✨ Pull in base OTel WebSDK (#3)
    * pull in webSDK code and connect it to example
    
    * make example app emit traces
    
    * remove mention of the session id span processor
    
    * ok nvm i figured out how to browserify the session id stuff

--- examples/hello-world-web/index.js
+++ examples/hello-world-web/index.js
@@ -1,7 +1,18 @@
 import { WebSDK } from '@honeycombio/opentelemetry-web';
+import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+
 const main = () => {
-  console.log('Hello from the example app');
-  new WebSDK();
+  // Set OTel to log in Debug mode
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+  // Initialize base OTel WebSDK
+  const sdk = new WebSDK({
+    instrumentations: [getWebAutoInstrumentations()], // add auto-instrumentation
+    traceExporter: new ConsoleSpanExporter(), // log spans to the console
+  });
+  sdk.start();
 };
 
 main();
