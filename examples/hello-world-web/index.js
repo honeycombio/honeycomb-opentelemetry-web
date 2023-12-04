8ace43c3addc7b8a757063b5e7b498289776e9af
Purvi Kanal <purvikanal@honeycomb.io>
2023-12-04 11:25:29 -0500

    make example app emit traces

--- examples/hello-world-web/index.js
+++ examples/hello-world-web/index.js
@@ -1,9 +1,17 @@
 import { WebSDK } from '@honeycombio/opentelemetry-web';
 import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+
 const main = () => {
-  console.log('Hello from the example app');
+  // Set OTel to log in Debug mode
   diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-  const sdk = new WebSDK();
+
+  // Initialize base OTel WebSDK
+  const sdk = new WebSDK({
+    instrumentations: [getWebAutoInstrumentations()], // add auto-instrumentation
+    traceExporter: new ConsoleSpanExporter(), // log spans to the console
+  });
   sdk.start();
 };
 
