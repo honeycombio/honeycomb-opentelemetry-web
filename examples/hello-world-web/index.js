e46fe76fa604d2ee4d9cd16207420c63cd0aafdc
Purvi Kanal <purvikanal@honeycomb.io>
2023-12-04 11:20:52 -0500

    pull in webSDK code and connect it to example

--- examples/hello-world-web/index.js
+++ examples/hello-world-web/index.js
@@ -1,7 +1,10 @@
 import { WebSDK } from '@honeycombio/opentelemetry-web';
+import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api';
 const main = () => {
   console.log('Hello from the example app');
-  new WebSDK();
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+  const sdk = new WebSDK();
+  sdk.start();
 };
 
 main();
