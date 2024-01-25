8ecee49eb1c23f9e461ef2e6cc2ebab8730c858e
Ashley Robinson <ashleyrobinson@honeycomb.io>
2024-01-25 10:28:27 -0500

    maint: 🔧  add `mergeResources` util (#31)
    Adds utility function to support merging an array of resources into a singular resource
    
    Moves the overhead of merging resources out of the individual configuration builders & into one centralized location: pass in an array of resources, get a singular resource w/ all the available attributes back

--- src/honeycomb-otel-sdk.ts
+++ src/honeycomb-otel-sdk.ts
@@ -2,11 +2,15 @@ import { WebSDK } from './base-otel-sdk';
 import { HoneycombOptions } from './types';
 import { configureHoneycombHttpJsonTraceExporter } from './http-json-trace-exporter';
 import { configureHoneycombResource } from './honeycomb-resource';
+import { mergeResources } from './merge-resources';
 
 export class HoneycombWebSDK extends WebSDK {
   constructor(options?: HoneycombOptions) {
     super({
-      resource: configureHoneycombResource(options),
+      resource: mergeResources([
+        configureHoneycombResource(),
+        options?.resource,
+      ]),
       traceExporter: configureHoneycombHttpJsonTraceExporter(options),
 
       ...options,
