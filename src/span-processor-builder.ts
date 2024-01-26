4548c7810c8c881e98c77624f748a9b523a68a9c
Purvi Kanal <purvikanal@honeycomb.io>
2024-01-26 10:12:39 -0500

    create composite span processor

--- src/span-processor-builder.ts
+++ src/span-processor-builder.ts
@@ -7,40 +7,44 @@ import {
 } from '@opentelemetry/sdk-trace-base';
 import { Context } from '@opentelemetry/api';
 
-export const configureSpanProcessor = (options?: HoneycombOptions) => {
-  if (options?.spanProcessor) {
-    return new CombinedSpanProcessor(options.spanProcessor);
-  }
+export const configureSpanProcessors = (options: HoneycombOptions) => {
+  const honeycombSpanProcessor = new CompositeSpanProcessor();
 
-  return new BrowserAttributesSpanProcessor();
+  honeycombSpanProcessor.addProcessor(new BrowserAttributesSpanProcessor());
+
+  if (options.spanProcessor) {
+    honeycombSpanProcessor.addProcessor(options.spanProcessor);
+  }
 };
 
-export class CombinedSpanProcessor implements SpanProcessor {
-  private customSpanProcessor: SpanProcessor;
-  private browserAttrsSpanProcessor: BrowserAttributesSpanProcessor;
+export class CompositeSpanProcessor implements SpanProcessor {
+  private spanProcessors: Array<SpanProcessor> = [];
 
-  constructor(customSpanProcessor: SpanProcessor) {
-    this.customSpanProcessor = customSpanProcessor;
-    this.browserAttrsSpanProcessor = new BrowserAttributesSpanProcessor();
+  public addProcessor(processor: SpanProcessor) {
+    this.spanProcessors.push(processor);
   }
 
   onStart(span: Span, parentContext: Context): void {
-    this.customSpanProcessor.onStart(span, parentContext);
-    this.browserAttrsSpanProcessor.onStart(span);
+    this.spanProcessors.forEach((processor) => {
+      processor.onStart(span, parentContext);
+    });
   }
 
   onEnd(span: ReadableSpan): void {
-    this.customSpanProcessor.onEnd(span);
-    this.browserAttrsSpanProcessor.onEnd();
+    this.spanProcessors.forEach((processor) => {
+      processor.onEnd(span);
+    });
   }
 
-  async forceFlush(): Promise<void> {
-    await this.customSpanProcessor.forceFlush();
-    await this.browserAttrsSpanProcessor.forceFlush();
+  forceFlush(): Promise<void> {
+    return Promise.all(
+      this.spanProcessors.map((processor) => processor.forceFlush()),
+    ).then(() => {});
   }
 
-  async shutdown(): Promise<void> {
-    await this.customSpanProcessor.shutdown();
-    await this.browserAttrsSpanProcessor.shutdown();
+  shutdown(): Promise<void> {
+    return Promise.all(
+      this.spanProcessors.map((processor) => processor.forceFlush()),
+    ).then(() => {});
   }
 }
