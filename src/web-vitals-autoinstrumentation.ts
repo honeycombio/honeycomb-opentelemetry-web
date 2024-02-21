488ce770c37cafba8aea5941de1ae4976f2242ca
Purvi Kanal <purvikanal@honeycomb.io>
2024-02-21 11:28:54 -0500

    refactor into separate functions for each vital

--- src/web-vitals-autoinstrumentation.ts
+++ src/web-vitals-autoinstrumentation.ts
@@ -9,7 +9,9 @@ import {
   INPMetricWithAttribution,
   LCPAttribution,
   LCPMetricWithAttribution,
+  Metric,
   onCLS,
+  onFCP,
   onFID,
   onINP,
   onLCP,
@@ -19,210 +21,160 @@ import {
 } from 'web-vitals/attribution';
 import { InstrumentationBase } from '@opentelemetry/instrumentation';
 
-type WebVitalWithAttribution =
-  | LCPMetricWithAttribution
-  | CLSMetricWithAttribution
-  | FIDMetricWithAttribution
-  | INPMetricWithAttribution
-  | FCPMetricWithAttribution
-  | TTFBMetricWithAttribution;
-
 export class WebVitalsInstrumentation extends InstrumentationBase {
   constructor() {
     super('@honeycombio/instrumentation-web-vitals', '0.0.1');
   }
   init() {}
 
-  private onReport(vital: WebVitalWithAttribution) {
-    const SPAN_ATTRIBUTE_PREFIX = 'web_vital';
+  private getSharedAttributes(vital: Metric) {
     const { name, id, delta, rating, value, navigationType } = vital;
+    return {
+      [`${name}.id`]: id,
+      [`${name}.delta`]: delta,
+      [`${name}.value`]: value,
+      [`${name}.rating`]: rating,
+      [`${name}.navigation_type`]: navigationType,
+    };
+  }
+
+  private onReportCLS(cls: CLSMetricWithAttribution) {
+    const { name, attribution } = cls;
+    const {
+      largestShiftTarget,
+      largestShiftTime,
+      largestShiftValue,
+      loadState,
+      largestShiftEntry,
+    }: CLSAttribution = attribution;
+
+    const span = this.tracer.startSpan(name);
+    span.setAttributes({
+      ...this.getSharedAttributes(cls),
+      [`${name}.largest_shift_target`]: largestShiftTarget,
+      [`${name}.element`]: largestShiftTarget,
+      [`${name}.largest_shift_time`]: largestShiftTime,
+      [`${name}.largest_shift_value`]: largestShiftValue,
+      [`${name}.load_state`]: loadState,
+      [`${name}.had_recent_input`]: largestShiftEntry?.hadRecentInput,
+    });
+
+    span.end();
+  }
+
+  private onReportLCP(lcp: LCPMetricWithAttribution) {
+    const { name, attribution } = lcp;
+    const {
+      element,
+      url,
+      timeToFirstByte,
+      resourceLoadDelay,
+      resourceLoadTime,
+      elementRenderDelay,
+    }: LCPAttribution = attribution;
+
+    const span = this.tracer.startSpan(name);
+    span.setAttributes({
+      ...this.getSharedAttributes(lcp),
+      [`${name}.element`]: element,
+      [`${name}.url`]: url,
+      [`${name}.time_to_first_byte`]: timeToFirstByte,
+      [`${name}.resource_load_delay`]: resourceLoadDelay,
+      [`${name}.resource_load_time`]: resourceLoadTime,
+      [`${name}.element_render_delay`]: elementRenderDelay,
+    });
+
+    span.end();
+  }
+
+  private onReportINP(inp: INPMetricWithAttribution) {
+    const { name, attribution } = inp;
+    const { eventTarget, eventType, loadState }: INPAttribution = attribution;
+
     const span = this.tracer.startSpan(name);
 
     span.setAttributes({
-      [`${SPAN_ATTRIBUTE_PREFIX}.name`]: name,
-      [`${SPAN_ATTRIBUTE_PREFIX}.id`]: id,
-      [`${SPAN_ATTRIBUTE_PREFIX}.delta`]: delta,
-      [`${SPAN_ATTRIBUTE_PREFIX}.value`]: value,
-      [`${SPAN_ATTRIBUTE_PREFIX}.rating`]: rating,
-      [`${SPAN_ATTRIBUTE_PREFIX}.navigation_type`]: navigationType,
+      ...this.getSharedAttributes(inp),
+      [`${name}.element`]: eventTarget,
+      [`${name}.event_type`]: eventType,
+      [`${name}.load_state`]: loadState,
     });
 
-    switch (name) {
-      case 'CLS': {
-        const {
-          largestShiftTarget,
-          largestShiftTime,
-          largestShiftValue,
-          loadState,
-          largestShiftEntry,
-        }: CLSAttribution = vital.attribution;
-        span.setAttributes({
-          'cls.largest_shift_target': largestShiftTarget,
-          'cls.element': largestShiftTarget,
-          'cls.largest_shift_time': largestShiftTime,
-          'cls.largest_shift_value': largestShiftValue,
-          'cls.load_state': loadState,
-          'cls.had_recent_input': largestShiftEntry?.hadRecentInput,
-        });
-        break;
-      }
-      case 'LCP': {
-        const {
-          element,
-          url,
-          timeToFirstByte,
-          resourceLoadDelay,
-          resourceLoadTime,
-          elementRenderDelay,
-        }: LCPAttribution = vital.attribution;
-        span.setAttributes({
-          'lcp.element': element,
-          'lcp.url': url,
-          'lcp.time_to_first_byte': timeToFirstByte,
-          'lcp.resource_load_delay': resourceLoadDelay,
-          'lcp.resource_load_time': resourceLoadTime,
-          'lcp.element_render_delay': elementRenderDelay,
-        });
-        break;
-      }
-      case 'INP': {
-        const { eventTarget, eventType, loadState }: INPAttribution =
-          vital.attribution;
+    span.end();
+  }
 
-        span.setAttributes({
-          'inp.element': eventTarget,
-          'inp.event_type': eventType,
-          'inp.load_state': loadState,
-        });
-        break;
-      }
-      case 'FCP': {
-        const { timeToFirstByte, firstByteToFCP, loadState }: FCPAttribution =
-          vital.attribution;
-        span.setAttributes({
-          'fcp.time_to_first_byte': timeToFirstByte,
-          'fcp.time_since_first_byte': firstByteToFCP,
-          'fcp.load_state': loadState,
-        });
-        break;
-      }
-      case 'TTFB': {
-        const {
-          waitingTime,
-          dnsTime,
-          connectionTime,
-          requestTime,
-        }: TTFBAttribution = vital.attribution;
-        span.setAttributes({
-          'ttfb.waiting_time': waitingTime,
-          'ttfb.dns_time': dnsTime,
-          'ttfb.connection_time': connectionTime,
-          'ttfb.request_time': requestTime,
-        });
-        break;
-      }
-      case 'FID': {
-        const { eventTarget, eventType, loadState }: FIDAttribution =
-          vital.attribution;
-        span.setAttributes({
-          'fid.element': eventTarget,
-          'fid.event_type': eventType,
-          'fid.load_state': loadState,
-        });
-      }
-    }
+  private onReportFCP(fcp: FCPMetricWithAttribution) {
+    const { name, attribution } = fcp;
+    const { timeToFirstByte, firstByteToFCP, loadState }: FCPAttribution =
+      attribution;
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      ...this.getSharedAttributes(fcp),
+      [`${name}.time_to_first_byte`]: timeToFirstByte,
+      [`${name}.time_since_first_byte`]: firstByteToFCP,
+      [`${name}.load_state`]: loadState,
+    });
+
+    span.end();
+  }
+
+  private onReportFID(fid: FIDMetricWithAttribution) {
+    const { name, attribution } = fid;
+    const { eventTarget, eventType, loadState }: FIDAttribution = attribution;
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      ...this.getSharedAttributes(fid),
+      [`${name}.element`]: eventTarget,
+      [`${name}.event_type`]: eventType,
+      [`${name}.load_state`]: loadState,
+    });
+
+    span.end();
+  }
+
+  private onReportTTFB(ttfb: TTFBMetricWithAttribution) {
+    const { name, attribution } = ttfb;
+    const {
+      waitingTime,
+      dnsTime,
+      connectionTime,
+      requestTime,
+    }: TTFBAttribution = attribution;
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      [`${name}.waiting_time`]: waitingTime,
+      [`${name}.dns_time`]: dnsTime,
+      [`${name}.connection_time`]: connectionTime,
+      [`${name}.request_time`]: requestTime,
+    });
+
+    span.end();
   }
 
   enable(): void {
     onCLS((vital) => {
-      this.onReport(vital);
+      this.onReportCLS(vital);
     });
     onFID((vital) => {
-      this.onReport(vital);
+      this.onReportFID(vital);
     });
     onLCP((vital) => {
-      this.onReport(vital);
+      this.onReportLCP(vital);
     });
     onINP((vital) => {
-      this.onReport(vital);
+      this.onReportINP(vital);
     });
     onTTFB((vital) => {
-      this.onReport(vital);
+      this.onReportTTFB(vital);
+    });
+    onFCP((vital) => {
+      this.onReportFCP(vital);
     });
   }
 }
-
-// export class WebVitalsInstrumentation extends InstrumentationBase {
-//   onReport(metric, parentSpanContext) {
-//     const now = hrTime();
-//     const webVitalsSpan = trace
-//       .getTracer('web-vitals-instrumentation')
-//       .startSpan(metric.name, { startTime: now }, parentSpanContext);
-
-//     webVitalsSpan.setAttributes({
-//       [`web_vital.name`]: metric.name,
-//       [`web_vital.id`]: metric.id,
-//       [`web_vital.navigationType`]: metric.navigationType,
-//       [`web_vital.delta`]: metric.delta,
-//       [`web_vital.rating`]: metric.rating,
-//       [`web_vital.value`]: metric.value,
-//       // can expand these into their own attributes!
-//       [`web_vital.entries`]: JSON.stringify(metric.entries),
-//     });
-//     switch (metric.name) {
-//       case 'LCP':
-//         webVitalsSpan.setAttributes({
-//           'web_vital.lcp.element': metric.attribution.element,
-//         });
-//         break;
-
-//       case 'CLS':
-//         webVitalsSpan.setAttributes({
-//           'web_vital.cls.largestShiftTarget':
-//             metric.attribution.largestShiftTarget,
-//         });
-//         break;
-//       case 'FID':
-//         webVitalsSpan.setAttributes({
-//           'web_vital.fid.eventTarget': metric.attribution.eventTarget,
-//         });
-//         break;
-//       case 'INP':
-//         webVitalsSpan.setAttributes({
-//           'web_vital.inp.eventTarget': metric.attribution.eventTarget,
-//         });
-//         break;
-
-//       default:
-//         break;
-//     }
-//     webVitalsSpan.end();
-//   }
-
-//   enable() {
-//     if (this.enabled) {
-//       return;
-//     }
-//     this.enabled = true;
-
-//     // create a parent span that will have all web vitals spans as children
-//     const parentSpan = trace
-//       .getTracer('web-vitals-instrumentation')
-//       .startSpan('web-vitals');
-//     const ctx = trace.setSpan(context.active(), parentSpan);
-//     parentSpan.end();
-
-//     onFID((metric) => {
-//       this.onReport(metric, ctx);
-//     });
-//     onCLS((metric) => {
-//       this.onReport(metric, ctx);
-//     });
-//     onLCP((metric) => {
-//       this.onReport(metric, ctx);
-//     });
-//     onINP((metric) => {
-//       this.onReport(metric, ctx);
-//     });
-//   }
-// }
