11580975a4425bb8e513f18062ac22f74eae606d
Wolfgang Therrien <wolfgangtherrien@honeycomb.io>
2024-05-09 12:41:11 -0400

    fix: Add ability to disable, don't clobber defaults when passing other config params. (#140)
    ## Which problem is this PR solving?
    
    When passing some config parameters, other defaults were not being
    honored.
    When `disable` 'd web vitals would still be reported.
    
    ## Short description of the changes
    Exposes a disable function and ensures that the call backs for
    web-vitals will not generate spans.
    Destructuring defaults will now supply defaults as expects when
    supplying only some configuration options.
    
    ## How to verify that this has the expected result
    Configure instrumentation with `{enabled: false}` you should not see
    spans generated.
    Configure instrumentation with `{vitalsToTrack: ['FID'],}` you should
    see other defaults still being honored i.e. `{enabled: true}`

--- src/web-vitals-autoinstrumentation.ts
+++ src/web-vitals-autoinstrumentation.ts
@@ -21,11 +21,22 @@ import {
   TTFBMetricWithAttribution,
 } from 'web-vitals/attribution';
 import {
-  InstrumentationBase,
+  Instrumentation,
   InstrumentationConfig,
 } from '@opentelemetry/instrumentation';
-import { Span } from '@opentelemetry/api';
 import { VERSION } from './version';
+import {
+  diag,
+  DiagLogger,
+  Meter,
+  MeterProvider,
+  metrics,
+  Span,
+  trace,
+  Tracer,
+  TracerProvider,
+} from '@opentelemetry/api';
+import * as shimmer from 'shimmer';
 
 type ApplyCustomAttributesFn = (vital: Metric, span: Span) => void;
 
@@ -43,6 +54,116 @@ interface VitalOpts extends ReportOpts {
    * }
    */
   applyCustomAttributes: ApplyCustomAttributesFn;
+}
+
+// To avoid importing InstrumentationAbstract from:
+// import { InstrumentationAbstract } from '@opentelemetry/instrumentation/build/src/instrumentation';
+// When this is exposed we can import from there.
+export abstract class InstrumentationAbstract implements Instrumentation {
+  protected _config: InstrumentationConfig;
+
+  private _tracer: Tracer;
+  private _meter: Meter;
+  protected _diag: DiagLogger;
+
+  constructor(
+    public readonly instrumentationName: string,
+    public readonly instrumentationVersion: string,
+    config: InstrumentationConfig = {},
+  ) {
+    this._config = {
+      enabled: true,
+      ...config,
+    };
+
+    this._diag = diag.createComponentLogger({
+      namespace: instrumentationName,
+    });
+
+    this._tracer = trace.getTracer(instrumentationName, instrumentationVersion);
+
+    this._meter = metrics.getMeter(instrumentationName, instrumentationVersion);
+    this._updateMetricInstruments();
+  }
+
+  /* Api to wrap instrumented method */
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  protected _wrap = shimmer.wrap;
+  /* Api to unwrap instrumented methods */
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  protected _unwrap = shimmer.unwrap;
+  /* Api to mass wrap instrumented method */
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  protected _massWrap = shimmer.massWrap;
+  /* Api to mass unwrap instrumented methods */
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  protected _massUnwrap = shimmer.massUnwrap;
+
+  /* Returns meter */
+  protected get meter(): Meter {
+    return this._meter;
+  }
+
+  /**
+   * Sets MeterProvider to this plugin
+   * @param meterProvider
+   */
+  public setMeterProvider(meterProvider: MeterProvider): void {
+    this._meter = meterProvider.getMeter(
+      this.instrumentationName,
+      this.instrumentationVersion,
+    );
+
+    this._updateMetricInstruments();
+  }
+
+  /**
+   * Sets the new metric instruments with the current Meter.
+   */
+  protected _updateMetricInstruments(): void {
+    return;
+  }
+
+  /* Returns InstrumentationConfig */
+  public getConfig(): InstrumentationConfig {
+    return this._config;
+  }
+
+  /**
+   * Sets InstrumentationConfig to this plugin
+   * @param InstrumentationConfig
+   */
+  public setConfig(config: InstrumentationConfig = {}): void {
+    this._config = Object.assign({}, config);
+  }
+
+  /**
+   * Sets TraceProvider to this plugin
+   * @param tracerProvider
+   */
+  public setTracerProvider(tracerProvider: TracerProvider): void {
+    this._tracer = tracerProvider.getTracer(
+      this.instrumentationName,
+      this.instrumentationVersion,
+    );
+  }
+
+  /* Returns tracer */
+  protected get tracer(): Tracer {
+    return this._tracer;
+  }
+
+  /* Disable plugin */
+  public abstract enable(): void;
+
+  /* Enable plugin */
+  public abstract disable(): void;
+
+  /**
+   * Init method in which plugin should define _modules and patches for
+   * methods
+   */
+  protected abstract init(): void;
 }
 
 export interface WebVitalsInstrumentationConfig extends InstrumentationConfig {
@@ -73,7 +194,7 @@ export interface WebVitalsInstrumentationConfig extends InstrumentationConfig {
  * Defaults to sending spans for CLS, LCP, INP, FCP and TTFB.
  * @param config The {@link WebVitalsInstrumentationConfig }
  */
-export class WebVitalsInstrumentation extends InstrumentationBase {
+export class WebVitalsInstrumentation extends InstrumentationAbstract {
   readonly vitalsToTrack: Array<Metric['name']>;
   readonly lcpOpts?: VitalOpts;
   readonly clsOpts?: VitalOpts;
@@ -81,237 +202,43 @@ export class WebVitalsInstrumentation extends InstrumentationBase {
   readonly fidOpts?: VitalOpts;
   readonly fcpOpts?: VitalOpts;
   readonly ttfbOpts?: VitalOpts;
-  readonly enabled?: boolean;
+  private _isEnabled: boolean;
 
-  constructor(
-    config: WebVitalsInstrumentationConfig = {
-      enabled: true,
-    },
-  ) {
-    super('@honeycombio/instrumentation-web-vitals', VERSION, {
-      /**
-       * NOTE: this is an unfortunate necessity to initially set
-       * the enabled state of the instrumentation to false because
-       * super gets called before anything else and in the parent class
-       * if enabled is true, it will call `this.enable()` and the `enable`
-       * function will run before any of the config options (e.g. vitalsToTrack)
-       * can become available. So we're setting this explicitly to false, making config options available,
-       * and then enabling the instrumentation in this constructor.
-       *
-       * This is usually not an issue when instrumentation is patching functions and not calling them
-       * directly, this instrumentation is a bit of a special case.
-       **/
-      enabled: false,
-    });
-    this.vitalsToTrack = config?.vitalsToTrack || [
-      'CLS',
-      'LCP',
-      'INP',
-      'FCP',
-      'TTFB',
-    ];
-    this.lcpOpts = config?.lcp;
-    this.clsOpts = config?.cls;
-    this.inpOpts = config?.inp;
-    this.fidOpts = config?.fid;
-    this.fcpOpts = config?.fcp;
-    this.ttfbOpts = config?.ttfb;
-    this.enabled = config?.enabled;
+  constructor({
+    enabled = true,
+    vitalsToTrack = ['CLS', 'LCP', 'INP'],
+    lcp,
+    cls,
+    inp,
+    fid,
+    fcp,
+    ttfb,
+  }: WebVitalsInstrumentationConfig = {}) {
+    const config: WebVitalsInstrumentationConfig = {
+      enabled,
+      vitalsToTrack,
+      lcp,
+      cls,
+      inp,
+      fid,
+      fcp,
+      ttfb,
+    };
+    super('@honeycombio/instrumentation-web-vitals', VERSION, config);
+    this.vitalsToTrack = [...vitalsToTrack];
+    this.lcpOpts = lcp;
+    this.clsOpts = cls;
+    this.inpOpts = inp;
+    this.fidOpts = fid;
+    this.fcpOpts = fcp;
+    this.ttfbOpts = ttfb;
+    this._isEnabled = enabled;
+    this._setupWebVitalsCallbacks();
   }
 
   init() {}
 
-  private getAttrPrefix(name: string) {
-    return name.toLowerCase();
-  }
-
-  private getSharedAttributes(vital: Metric) {
-    const { name, id, delta, rating, value, navigationType } = vital;
-    const attrPrefix = this.getAttrPrefix(name);
-    return {
-      [`${attrPrefix}.id`]: id,
-      [`${attrPrefix}.delta`]: delta,
-      [`${attrPrefix}.value`]: value,
-      [`${attrPrefix}.rating`]: rating,
-      [`${attrPrefix}.navigation_type`]: navigationType,
-    };
-  }
-
-  onReportCLS = (
-    cls: CLSMetricWithAttribution,
-    applyCustomAttributes?: ApplyCustomAttributesFn,
-  ) => {
-    const { name, attribution } = cls;
-    const {
-      largestShiftTarget,
-      largestShiftTime,
-      largestShiftValue,
-      loadState,
-      largestShiftEntry,
-    }: CLSAttribution = attribution;
-    const attrPrefix = this.getAttrPrefix(name);
-
-    const span = this.tracer.startSpan(name);
-    span.setAttributes({
-      ...this.getSharedAttributes(cls),
-      [`${attrPrefix}.largest_shift_target`]: largestShiftTarget,
-      [`${attrPrefix}.element`]: largestShiftTarget,
-      [`${attrPrefix}.largest_shift_time`]: largestShiftTime,
-      [`${attrPrefix}.largest_shift_value`]: largestShiftValue,
-      [`${attrPrefix}.load_state`]: loadState,
-      [`${attrPrefix}.had_recent_input`]: largestShiftEntry?.hadRecentInput,
-    });
-
-    if (applyCustomAttributes) {
-      applyCustomAttributes(cls, span);
-    }
-
-    span.end();
-  };
-
-  onReportLCP = (
-    lcp: LCPMetricWithAttribution,
-    applyCustomAttributes?: ApplyCustomAttributesFn,
-  ) => {
-    const { name, attribution } = lcp;
-    const {
-      element,
-      url,
-      timeToFirstByte,
-      resourceLoadDelay,
-      resourceLoadTime,
-      elementRenderDelay,
-    }: LCPAttribution = attribution;
-    const attrPrefix = this.getAttrPrefix(name);
-
-    const span = this.tracer.startSpan(name);
-    span.setAttributes({
-      ...this.getSharedAttributes(lcp),
-      [`${attrPrefix}.element`]: element,
-      [`${attrPrefix}.url`]: url,
-      [`${attrPrefix}.time_to_first_byte`]: timeToFirstByte,
-      [`${attrPrefix}.resource_load_delay`]: resourceLoadDelay,
-      [`${attrPrefix}.resource_load_time`]: resourceLoadTime,
-      [`${attrPrefix}.element_render_delay`]: elementRenderDelay,
-    });
-
-    if (applyCustomAttributes) {
-      applyCustomAttributes(lcp, span);
-    }
-
-    span.end();
-  };
-
-  onReportINP = (
-    inp: INPMetricWithAttribution,
-    applyCustomAttributes?: ApplyCustomAttributesFn,
-  ) => {
-    const { name, attribution } = inp;
-    const { eventTarget, eventType, loadState }: INPAttribution = attribution;
-    const attrPrefix = this.getAttrPrefix(name);
-
-    const span = this.tracer.startSpan(name);
-
-    span.setAttributes({
-      ...this.getSharedAttributes(inp),
-      [`${attrPrefix}.element`]: eventTarget,
-      [`${attrPrefix}.event_type`]: eventType,
-      [`${attrPrefix}.load_state`]: loadState,
-    });
-
-    if (applyCustomAttributes) {
-      applyCustomAttributes(inp, span);
-    }
-
-    span.end();
-  };
-
-  onReportFCP = (
-    fcp: FCPMetricWithAttribution,
-    applyCustomAttributes?: ApplyCustomAttributesFn,
-  ) => {
-    const { name, attribution } = fcp;
-    const { timeToFirstByte, firstByteToFCP, loadState }: FCPAttribution =
-      attribution;
-    const attrPrefix = this.getAttrPrefix(name);
-
-    const span = this.tracer.startSpan(name);
-
-    span.setAttributes({
-      ...this.getSharedAttributes(fcp),
-      [`${attrPrefix}.time_to_first_byte`]: timeToFirstByte,
-      [`${attrPrefix}.time_since_first_byte`]: firstByteToFCP,
-      [`${attrPrefix}.load_state`]: loadState,
-    });
-
-    if (applyCustomAttributes) {
-      applyCustomAttributes(fcp, span);
-    }
-
-    span.end();
-  };
-
-  onReportFID = (
-    fid: FIDMetricWithAttribution,
-    applyCustomAttributes?: ApplyCustomAttributesFn,
-  ) => {
-    const { name, attribution } = fid;
-    const { eventTarget, eventType, loadState }: FIDAttribution = attribution;
-    const attrPrefix = this.getAttrPrefix(name);
-
-    const span = this.tracer.startSpan(name);
-
-    span.setAttributes({
-      ...this.getSharedAttributes(fid),
-      [`${attrPrefix}.element`]: eventTarget,
-      [`${attrPrefix}.event_type`]: eventType,
-      [`${attrPrefix}.load_state`]: loadState,
-    });
-
-    if (applyCustomAttributes) {
-      applyCustomAttributes(fid, span);
-    }
-
-    span.end();
-  };
-
-  onReportTTFB = (
-    ttfb: TTFBMetricWithAttribution,
-    applyCustomAttributes?: ApplyCustomAttributesFn,
-  ) => {
-    const { name, attribution } = ttfb;
-    const {
-      waitingTime,
-      dnsTime,
-      connectionTime,
-      requestTime,
-    }: TTFBAttribution = attribution;
-    const attrPrefix = this.getAttrPrefix(name);
-
-    const span = this.tracer.startSpan(name);
-
-    span.setAttributes({
-      ...this.getSharedAttributes(ttfb),
-      [`${attrPrefix}.waiting_time`]: waitingTime,
-      [`${attrPrefix}.dns_time`]: dnsTime,
-      [`${attrPrefix}.connection_time`]: connectionTime,
-      [`${attrPrefix}.request_time`]: requestTime,
-    });
-
-    if (applyCustomAttributes) {
-      applyCustomAttributes(ttfb, span);
-    }
-
-    span.end();
-  };
-
-  enable(): void {
-    if (!this.enabled) {
-      this._diag.debug(`Instrumentation disabled`);
-      return;
-    }
-    this._diag.debug(`Sending spans for ${this.vitalsToTrack.join(',')}`);
-
+  private _setupWebVitalsCallbacks() {
     if (this.vitalsToTrack.includes('CLS')) {
       onCLS((vital) => {
         this.onReportCLS(vital, this.clsOpts?.applyCustomAttributes);
@@ -347,5 +274,223 @@ export class WebVitalsInstrumentation extends InstrumentationBase {
         this.onReportFCP(vital, this.fcpOpts?.applyCustomAttributes);
       }, this.fcpOpts);
     }
+  }
+
+  private getAttrPrefix(name: string) {
+    return name.toLowerCase();
+  }
+
+  private getSharedAttributes(vital: Metric) {
+    const { name, id, delta, rating, value, navigationType } = vital;
+    const attrPrefix = this.getAttrPrefix(name);
+    return {
+      [`${attrPrefix}.id`]: id,
+      [`${attrPrefix}.delta`]: delta,
+      [`${attrPrefix}.value`]: value,
+      [`${attrPrefix}.rating`]: rating,
+      [`${attrPrefix}.navigation_type`]: navigationType,
+    };
+  }
+
+  onReportCLS = (
+    cls: CLSMetricWithAttribution,
+    applyCustomAttributes?: ApplyCustomAttributesFn,
+  ) => {
+    if (!this.isEnabled()) return;
+
+    const { name, attribution } = cls;
+    const {
+      largestShiftTarget,
+      largestShiftTime,
+      largestShiftValue,
+      loadState,
+      largestShiftEntry,
+    }: CLSAttribution = attribution;
+    const attrPrefix = this.getAttrPrefix(name);
+
+    const span = this.tracer.startSpan(name);
+    span.setAttributes({
+      ...this.getSharedAttributes(cls),
+      [`${attrPrefix}.largest_shift_target`]: largestShiftTarget,
+      [`${attrPrefix}.element`]: largestShiftTarget,
+      [`${attrPrefix}.largest_shift_time`]: largestShiftTime,
+      [`${attrPrefix}.largest_shift_value`]: largestShiftValue,
+      [`${attrPrefix}.load_state`]: loadState,
+      [`${attrPrefix}.had_recent_input`]: largestShiftEntry?.hadRecentInput,
+    });
+
+    if (applyCustomAttributes) {
+      applyCustomAttributes(cls, span);
+    }
+
+    span.end();
+  };
+
+  onReportLCP = (
+    lcp: LCPMetricWithAttribution,
+    applyCustomAttributes?: ApplyCustomAttributesFn,
+  ) => {
+    if (!this.isEnabled()) return;
+
+    const { name, attribution } = lcp;
+    const {
+      element,
+      url,
+      timeToFirstByte,
+      resourceLoadDelay,
+      resourceLoadTime,
+      elementRenderDelay,
+    }: LCPAttribution = attribution;
+    const attrPrefix = this.getAttrPrefix(name);
+
+    const span = this.tracer.startSpan(name);
+    span.setAttributes({
+      ...this.getSharedAttributes(lcp),
+      [`${attrPrefix}.element`]: element,
+      [`${attrPrefix}.url`]: url,
+      [`${attrPrefix}.time_to_first_byte`]: timeToFirstByte,
+      [`${attrPrefix}.resource_load_delay`]: resourceLoadDelay,
+      [`${attrPrefix}.resource_load_time`]: resourceLoadTime,
+      [`${attrPrefix}.element_render_delay`]: elementRenderDelay,
+    });
+
+    if (applyCustomAttributes) {
+      applyCustomAttributes(lcp, span);
+    }
+
+    span.end();
+  };
+
+  onReportINP = (
+    inp: INPMetricWithAttribution,
+    applyCustomAttributes?: ApplyCustomAttributesFn,
+  ) => {
+    if (!this.isEnabled()) return;
+
+    const { name, attribution } = inp;
+    const { eventTarget, eventType, loadState }: INPAttribution = attribution;
+    const attrPrefix = this.getAttrPrefix(name);
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      ...this.getSharedAttributes(inp),
+      [`${attrPrefix}.element`]: eventTarget,
+      [`${attrPrefix}.event_type`]: eventType,
+      [`${attrPrefix}.load_state`]: loadState,
+    });
+
+    if (applyCustomAttributes) {
+      applyCustomAttributes(inp, span);
+    }
+
+    span.end();
+  };
+
+  onReportFCP = (
+    fcp: FCPMetricWithAttribution,
+    applyCustomAttributes?: ApplyCustomAttributesFn,
+  ) => {
+    if (!this.isEnabled()) return;
+
+    const { name, attribution } = fcp;
+    const { timeToFirstByte, firstByteToFCP, loadState }: FCPAttribution =
+      attribution;
+    const attrPrefix = this.getAttrPrefix(name);
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      ...this.getSharedAttributes(fcp),
+      [`${attrPrefix}.time_to_first_byte`]: timeToFirstByte,
+      [`${attrPrefix}.time_since_first_byte`]: firstByteToFCP,
+      [`${attrPrefix}.load_state`]: loadState,
+    });
+
+    if (applyCustomAttributes) {
+      applyCustomAttributes(fcp, span);
+    }
+
+    span.end();
+  };
+
+  onReportFID = (
+    fid: FIDMetricWithAttribution,
+    applyCustomAttributes?: ApplyCustomAttributesFn,
+  ) => {
+    if (!this.isEnabled()) return;
+
+    const { name, attribution } = fid;
+    const { eventTarget, eventType, loadState }: FIDAttribution = attribution;
+    const attrPrefix = this.getAttrPrefix(name);
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      ...this.getSharedAttributes(fid),
+      [`${attrPrefix}.element`]: eventTarget,
+      [`${attrPrefix}.event_type`]: eventType,
+      [`${attrPrefix}.load_state`]: loadState,
+    });
+
+    if (applyCustomAttributes) {
+      applyCustomAttributes(fid, span);
+    }
+
+    span.end();
+  };
+
+  onReportTTFB = (
+    ttfb: TTFBMetricWithAttribution,
+    applyCustomAttributes?: ApplyCustomAttributesFn,
+  ) => {
+    if (!this.isEnabled()) return;
+
+    const { name, attribution } = ttfb;
+    const {
+      waitingTime,
+      dnsTime,
+      connectionTime,
+      requestTime,
+    }: TTFBAttribution = attribution;
+    const attrPrefix = this.getAttrPrefix(name);
+
+    const span = this.tracer.startSpan(name);
+
+    span.setAttributes({
+      ...this.getSharedAttributes(ttfb),
+      [`${attrPrefix}.waiting_time`]: waitingTime,
+      [`${attrPrefix}.dns_time`]: dnsTime,
+      [`${attrPrefix}.connection_time`]: connectionTime,
+      [`${attrPrefix}.request_time`]: requestTime,
+    });
+
+    if (applyCustomAttributes) {
+      applyCustomAttributes(ttfb, span);
+    }
+
+    span.end();
+  };
+
+  disable(): void {
+    if (!this.isEnabled()) {
+      this._diag.debug(`Instrumentation already disabled`);
+      return;
+    }
+    this._isEnabled = false;
+    this._diag.debug(`Instrumentation  disabled`);
+  }
+
+  enable(): void {
+    if (this.isEnabled()) {
+      this._diag.debug(`Instrumentation already enabled`);
+      return;
+    }
+    this._isEnabled = true;
+    this._diag.debug(`Instrumentation  enabled`);
+    this._diag.debug(`Sending spans for ${this.vitalsToTrack.join(',')}`);
+  }
+  public isEnabled() {
+    return this._isEnabled;
   }
 }
