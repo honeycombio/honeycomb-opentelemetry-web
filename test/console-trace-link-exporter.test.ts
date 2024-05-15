e8bf8a42cc0303c065049b9acd9ef3b2a66c2958
Mustafa Haddara <mustafa@honeycomb.io>
2024-05-15 15:52:11 -0400

    feat: drop axios requirement (#151)
    ## Which problem is this PR solving?
    Some users were unable to install this package due to issues with our
    transitive dependency on `axios`.
    
    It turns out, though, that we don't really need axios for much. This PR
    removes it.
    
    ## Short description of the changes
    Drop axios, switch to standard `fetch` API
    
    ## How to verify that this has the expected result
    - [x] unit tests pass
    - [x] Install into test repo, confirm ConsoleTraceLinkExporter still
    does what we think it does

--- test/console-trace-link-exporter.test.ts
+++ test/console-trace-link-exporter.test.ts
@@ -1,4 +1,8 @@
-import { buildTraceUrl } from '../src/console-trace-link-exporter';
+import { ReadableSpan } from '@opentelemetry/sdk-trace-base';
+import {
+  buildTraceUrl,
+  configureConsoleTraceLinkExporter,
+} from '../src/console-trace-link-exporter';
 
 const apikey = '000000000000000000000000';
 const classicApikey = '00000000000000000000000000000000';
@@ -25,6 +29,151 @@ describe('buildTraceUrl', () => {
     );
     expect(url).toBe(
       'https://ui.honeycomb.io/my-team/datasets/my-service/trace?trace_id',
+    );
+  });
+});
+
+describe('ConsoleTraceLinkExporter', () => {
+  let originalFetch: typeof global.fetch;
+  let consoleLogSpy: jest.SpyInstance;
+
+  beforeAll(() => {
+    originalFetch = global.fetch;
+    global.fetch = jest.fn();
+
+    consoleLogSpy = jest.spyOn(console, 'log');
+  });
+  afterAll(() => {
+    global.fetch = originalFetch;
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('makes request and logs the trace url', async () => {
+    (global.fetch as jest.Mock).mockResolvedValueOnce({
+      ok: true,
+      json: () =>
+        Promise.resolve({
+          team: { slug: 'test-team' },
+          environment: { slug: 'test-env' },
+        }),
+    });
+
+    const exporter = configureConsoleTraceLinkExporter({
+      apiKey: apikey,
+      serviceName: 'test-service',
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://api.honeycomb.io/1/auth',
+      {
+        headers: {
+          'x-honeycomb-team': apikey,
+        },
+      },
+    );
+
+    // @ts-expect-error we only include the properties we need
+    const fakeSpan: ReadableSpan = {
+      spanContext: () => ({
+        traceId: '1234',
+        spanId: '1234',
+        traceFlags: 0,
+      }),
+    };
+
+    // wait for promises to resolve
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    await new Promise(process.nextTick);
+
+    // all of the fetch() promises have to resolve before we can export
+    exporter.export([fakeSpan], () => {});
+
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      '@honeycombio/opentelemetry-web: Honeycomb link: https://ui.honeycomb.io/test-team/environments/test-env/datasets/test-service/trace?trace_id=1234',
+    );
+  });
+
+  it('makes request and handles successful requests that return errors', async () => {
+    (global.fetch as jest.Mock).mockResolvedValueOnce({
+      ok: false,
+    });
+
+    const exporter = configureConsoleTraceLinkExporter({
+      apiKey: apikey,
+      serviceName: 'test-service',
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://api.honeycomb.io/1/auth',
+      {
+        headers: {
+          'x-honeycomb-team': apikey,
+        },
+      },
+    );
+
+    // @ts-expect-error we only include the properties we need
+    const fakeSpan: ReadableSpan = {
+      spanContext: () => ({
+        traceId: '1234',
+        spanId: '1234',
+        traceFlags: 0,
+      }),
+    };
+
+    // wait for promises to resolve
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    await new Promise(process.nextTick);
+
+    // all of the fetch() promises have to resolve before we can export
+    exporter.export([fakeSpan], () => {});
+
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      '@honeycombio/opentelemetry-web: 🔕 Failed to get proper auth response from Honeycomb. No local visualization available.',
+    );
+  });
+
+  it('makes request and handles failed requests', async () => {
+    (global.fetch as jest.Mock).mockRejectedValueOnce({});
+
+    const exporter = configureConsoleTraceLinkExporter({
+      apiKey: apikey,
+      serviceName: 'test-service',
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://api.honeycomb.io/1/auth',
+      {
+        headers: {
+          'x-honeycomb-team': apikey,
+        },
+      },
+    );
+
+    // @ts-expect-error we only include the properties we need
+    const fakeSpan: ReadableSpan = {
+      spanContext: () => ({
+        traceId: '1234',
+        spanId: '1234',
+        traceFlags: 0,
+      }),
+    };
+
+    // wait for promises to resolve
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    await new Promise(process.nextTick);
+
+    // all of the fetch() promises have to resolve before we can export
+    exporter.export([fakeSpan], () => {});
+
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      '@honeycombio/opentelemetry-web: 🔕 Failed to get proper auth response from Honeycomb. No local visualization available.',
     );
   });
 });
