89ee68f1b214dbaf2393b9fe76890d6cd40da58f
Wolfgang Therrien <wolfgangtherrien@honeycomb.io>
2024-07-09 16:50:04 -0400

    Ignore RCA unit tests.

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,5 @@ module.exports = {
   transformIgnorePatterns: ['^.+\\.js$'],
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/examples/hello-world-react-create-app/'],
 };
