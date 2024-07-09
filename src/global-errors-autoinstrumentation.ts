fa5037df41a77d50d334a0dfffbeec43cf2d5191
Wolfgang Therrien <wolfgangtherrien@honeycomb.io>
2024-07-09 17:01:53 -0400

    feat: Add Global Error Instrumentation (#186)
    A few additions to [@nordfjor](https://github.com/nordfjord)'s excellent
    contribution here #181
    
    ## Which problem is this PR solving?
    
    Sometimes errors escape and it's useful to know what those errors were.
    OpenTelemetry suggests these should go on the log signal, which does
    make sense, but honeycomb doesn't really use that signal so it makes
    sense for this to live in the honeycomb distribution.
    
    ## Short description of the changes
    
    - Adds a new `GlobalErrorsInstrumentation`
    - when enabled it attaches handlers to `unhandledrejection` and `error`
    events on the window
    - The handler creates a span that matches the exception conventions from
    opentelemetry
    
    ## How to verify that this has the expected result
    - Run the tests
    - Use the sample Hello World app to send an uncaught error to Honeycomb
    
    ## Complications
    - The jest environment is slightly different from actual browsers so I
    was unable to test the unhandled rejection flow, but have left a test
    that would work in an actual browser environment (marked as `it.skip` to
    avoid running it)
    - Could not test with the instrumentation disabled because jest will
    fail any test that leaks an error automatically
    
    ---------
    
    Co-authored-by: Einar Norðfjörð <gloogankle@gmail.com>

---
+++ src/global-errors-autoinstrumentation.ts
@@ -0,0 +1,76 @@
+import { InstrumentationConfig } from '@opentelemetry/instrumentation';
+import { InstrumentationAbstract } from './web-vitals-autoinstrumentation';
+import { VERSION } from './version';
+import { context, SpanStatusCode } from '@opentelemetry/api';
+import {
+  SEMATTRS_EXCEPTION_MESSAGE,
+  SEMATTRS_EXCEPTION_STACKTRACE,
+  SEMATTRS_EXCEPTION_TYPE,
+} from '@opentelemetry/semantic-conventions';
+
+export interface GlobalErrorsInstrumentationConfig
+  extends InstrumentationConfig {}
+
+/**
+ * Global errors auto-instrumentation, sends spans automatically for exceptions that reach the window.
+ * @param config The {@link GlobalErrorsInstrumentationConfig}
+ */
+export class GlobalErrorsInstrumentation extends InstrumentationAbstract {
+  private _isEnabled: boolean;
+  constructor({ enabled = true }: GlobalErrorsInstrumentationConfig = {}) {
+    const config: GlobalErrorsInstrumentationConfig = { enabled };
+    super('@honeycombio/instrumentation-global-errors', VERSION, config);
+    if (enabled) {
+      this.enable();
+    }
+    this._isEnabled = enabled;
+  }
+
+  onError = (event: ErrorEvent | PromiseRejectionEvent) => {
+    const error: Error | undefined =
+      'reason' in event ? event.reason : event.error;
+    const message = error?.message;
+    const type = error?.name;
+    const attributes = {
+      [SEMATTRS_EXCEPTION_TYPE]: type,
+      [SEMATTRS_EXCEPTION_MESSAGE]: message,
+      [SEMATTRS_EXCEPTION_STACKTRACE]: error?.stack,
+    };
+    // otel spec requires at minimum these two
+    if (!message || !type) return;
+    const errorSpan = this.tracer.startSpan(
+      'exception',
+      { attributes },
+      context.active(),
+    );
+    errorSpan.setStatus({ code: SpanStatusCode.ERROR, message });
+    errorSpan.end();
+  };
+
+  init() {}
+
+  disable(): void {
+    if (!this.isEnabled()) {
+      this._diag.debug(`Instrumentation already disabled`);
+      return;
+    }
+    this._isEnabled = false;
+    window.removeEventListener('error', this.onError);
+    window.removeEventListener('unhandledrejection', this.onError);
+    this._diag.debug(`Instrumentation  disabled`);
+  }
+
+  enable(): void {
+    if (this.isEnabled()) {
+      this._diag.debug(`Instrumentation already enabled`);
+      return;
+    }
+    this._isEnabled = true;
+    window.addEventListener('error', this.onError);
+    window.addEventListener('unhandledrejection', this.onError);
+    this._diag.debug(`Instrumentation  enabled`);
+  }
+  public isEnabled() {
+    return this._isEnabled;
+  }
+}
