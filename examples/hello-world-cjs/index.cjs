7cefca87867e9af59fcc018760c442c4396d541c
Wolfgang Therrien <wolfgangtherrien@honeycomb.io>
2024-07-10 11:35:28 -0400

    Update examples/hello-world-cjs/index.cjs

--- examples/hello-world-cjs/index.cjs
+++ examples/hello-world-cjs/index.cjs
@@ -12,7 +12,7 @@ const main = () => {
   const sdk = new HoneycombWebSDK({
     // endpoint: 'http://localhost:4318', // send to collector
     // To send to collector, comment out API Key
-    apiKey: 'hcaik_01hxc8g4cdhrywa0n9132mhfwh6yekhecstp429k90rec31qhv5cfjn5wk',
+    apiKey: 'api-key',
     serviceName: 'web-distro-basic-example',
     debug: true,
     instrumentations: [
