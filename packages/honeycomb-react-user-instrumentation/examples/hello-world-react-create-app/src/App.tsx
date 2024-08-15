a03475272ca287fe2193cb337875e54a034ae75c
Mustafa Haddara <mustafa@honeycomb.io>
2024-08-15 16:48:57 -0400

    add example app

---
+++ packages/honeycomb-react-user-instrumentation/examples/hello-world-react-create-app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {new Date().toLocaleDateString('en-us') +
+            ' ' +
+            new Date().toLocaleTimeString('en-us')}
+        </p>
+        <p>words words words</p>
+
+        <button type="button" onClick={() => console.log('click')}>click me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
