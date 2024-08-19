e6db5ca0b44e26489fd6cb33507d0cadb39e0e63
Mustafa Haddara <mustafa@honeycomb.io>
2024-08-19 12:21:56 -0400

    allow clients to pass list of event names

--- packages/honeycomb-react-user-instrumentation/src/ReactUserInteractionInstrumentation.ts
+++ packages/honeycomb-react-user-instrumentation/src/ReactUserInteractionInstrumentation.ts
@@ -1,21 +1,27 @@
 import {
+  EventName,
   UserInteractionInstrumentation,
   UserInteractionInstrumentationConfig,
 } from '@opentelemetry/instrumentation-user-interaction';
 
-const DEFAULT_EVENT_NAMES = ['react-click'];
+const DEFAULT_EVENT_NAMES = ['click'];
+
+type ReactPrefix<K> = K extends string ? `react-${K}` : K;
+type ReactEventName = ReactPrefix<EventName>;
+
+const buildEventNames = (events: EventName[]): ReactEventName[] => {
+  return events.map((ev) => `react-${ev}` as ReactEventName);
+};
 
 export class ReactUserInteractionInstrumentation extends UserInteractionInstrumentation {
-  constructor(
-    config: Omit<UserInteractionInstrumentationConfig, 'eventNames'> = {},
-  ) {
+  constructor(config: UserInteractionInstrumentationConfig = {}) {
     super({
       ...config,
       // React sets up its own event system, with its own event names.
       //    We intentionally pass through non-browser events here so we can listen
       //    to the custom events.
       // @ts-expect-error React has its own event names
-      eventNames: DEFAULT_EVENT_NAMES,
+      eventNames: buildEventNames(config.eventNames ?? DEFAULT_EVENT_NAMES),
     });
   }
 }
