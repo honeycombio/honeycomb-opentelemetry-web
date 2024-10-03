3d0c4eab10805fdca9222a75da3fee781be8fce3
Arriana Blais <arriblais@honeycomb.io>
2024-10-03 09:45:32 -0700

    Don't expose internal function

--- packages/honeycomb-opentelemetry-web/src/index.ts
+++ packages/honeycomb-opentelemetry-web/src/index.ts
@@ -1,4 +1,3 @@
 export * from './base-otel-sdk';
 export * from './honeycomb-otel-sdk';
 export * from './web-vitals-autoinstrumentation';
-export { configureHoneycombHttpJsonTraceExporter as getDefaultTraceExporter } from './http-json-trace-exporter';
