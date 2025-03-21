3efcbc43ea1965b3a89efdc3013fe399488b163e
Martin Holman <me@martinholman.co.nz>
2025-03-21 13:04:58 -0700

    Ensure we return the same sessionid each time

--- packages/honeycomb-opentelemetry-web/src/default-session-provider.ts
+++ packages/honeycomb-opentelemetry-web/src/default-session-provider.ts
@@ -1,18 +1,8 @@
-const SESSION_ID_BYTES = 16;
-const SHARED_CHAR_CODES_ARRAY = Array(32);
+import { RandomIdGenerator } from "@opentelemetry/sdk-trace-base";
+
+const generator = new RandomIdGenerator();
+const sessionId = generator.generateTraceId();
 
 export const defaultSessionProvider = {
-  getSessionId: () => {
-    for (let i = 0; i < SESSION_ID_BYTES * 2; i++) {
-      SHARED_CHAR_CODES_ARRAY[i] = Math.floor(Math.random() * 16) + 48;
-      // valid hex characters in the range 48-57 and 97-102
-      if (SHARED_CHAR_CODES_ARRAY[i] >= 58) {
-        SHARED_CHAR_CODES_ARRAY[i] += 39;
-      }
-    }
-    return String.fromCharCode.apply(
-      null,
-      SHARED_CHAR_CODES_ARRAY.slice(0, SESSION_ID_BYTES * 2),
-    );
-  },
+  getSessionId: () => (sessionId),
 };
