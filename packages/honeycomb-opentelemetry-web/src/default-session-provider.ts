f7366afcc50db892bedbe5f2e6c5fa5bcfa460dd
Martin Holman <me@martinholman.co.nz>
2025-03-21 13:17:38 -0700

    💅 format

--- packages/honeycomb-opentelemetry-web/src/default-session-provider.ts
+++ packages/honeycomb-opentelemetry-web/src/default-session-provider.ts
@@ -1,8 +1,8 @@
-import { RandomIdGenerator } from "@opentelemetry/sdk-trace-base";
+import { RandomIdGenerator } from '@opentelemetry/sdk-trace-base';
 
 const generator = new RandomIdGenerator();
 const sessionId = generator.generateTraceId();
 
 export const defaultSessionProvider = {
-  getSessionId: () => (sessionId),
+  getSessionId: () => sessionId,
 };
