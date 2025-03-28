0700f56087643ee6b94c9b63bea238f2a26487c8
Purvi Kanal <purvikanal@honeycomb.io>
2025-03-28 12:40:08 -0400

    feat: Export types and BaggageSpanProcessor (#502)
    ## Which problem is this PR solving?
    Exports SDK types and the BaggaeSpanProcessor as an individual
    component.
    
    ## How to verify that this has the expected result
    - Can import types and BaggageSpanProcessor individually.

--- packages/honeycomb-opentelemetry-web/src/index.ts
+++ packages/honeycomb-opentelemetry-web/src/index.ts
@@ -3,3 +3,5 @@ export * from './honeycomb-otel-sdk';
 export * from './web-vitals-autoinstrumentation';
 export * from './global-errors-autoinstrumentation';
 export * from './experimental';
+export * from './baggage-span-processor';
+export * from './types';
