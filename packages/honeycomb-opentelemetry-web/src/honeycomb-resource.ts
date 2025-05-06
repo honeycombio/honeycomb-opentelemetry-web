84e3c440b2eeedb962163fe640dcdd9cc492b06a
Martin Holman <me@martinholman.co.nz>
2025-05-06 13:17:45 -0700

    Add semantic distro attributes

--- packages/honeycomb-opentelemetry-web/src/honeycomb-resource.ts
+++ packages/honeycomb-opentelemetry-web/src/honeycomb-resource.ts
@@ -1,9 +1,12 @@
 import { Resource, resourceFromAttributes } from '@opentelemetry/resources';
+import { ATTR_TELEMETRY_DISTRO_NAME, ATTR_TELEMETRY_DISTRO_VERSION } from '@opentelemetry/semantic-conventions/incubating';
 import { VERSION } from './version';
 
 export function configureHoneycombResource(): Resource {
   return resourceFromAttributes({
     'honeycomb.distro.version': VERSION,
     'honeycomb.distro.runtime_version': 'browser',
+    [ATTR_TELEMETRY_DISTRO_NAME]: '@honeycombio/opentelemetry-web',
+    [ATTR_TELEMETRY_DISTRO_VERSION]: VERSION,
   });
 }
