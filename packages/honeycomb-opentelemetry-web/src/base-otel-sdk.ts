8fe815ce5d1d6c75b1ea85e5eb3808471eecd337
Bee Klimt <beeklimt@honeycomb.io>
2025-09-14 14:18:37 -0700

    fix: fix instrumentation to get correct tracer

--- packages/honeycomb-opentelemetry-web/src/base-otel-sdk.ts
+++ packages/honeycomb-opentelemetry-web/src/base-otel-sdk.ts
@@ -17,7 +17,7 @@
  * limitations under the License.
  */
 
-import { ContextManager, metrics, TextMapPropagator } from '@opentelemetry/api';
+import { ContextManager, metrics, TextMapPropagator, trace } from '@opentelemetry/api';
 import {
   Instrumentation,
   registerInstrumentations,
@@ -169,10 +169,6 @@ export class WebSDK {
       return;
     }
 
-    registerInstrumentations({
-      instrumentations: this._instrumentations,
-    });
-
     if (this._autoDetectResources) {
       const internalConfig: ResourceDetectionConfig = {
         detectors: this._resourceDetectors,
@@ -220,6 +216,7 @@ export class WebSDK {
       contextManager: this._tracerProviderConfig?.contextManager,
       propagator: this._tracerProviderConfig?.textMapPropagator,
     });
+    trace.setGlobalTracerProvider(tracerProvider);
 
     if (this._meterProviderConfig) {
       const readers = this._meterProviderConfig.metricExporters.map(
@@ -246,6 +243,10 @@ export class WebSDK {
       });
       logs.setGlobalLoggerProvider(this._loggerProvider);
     }
+
+    registerInstrumentations({
+      instrumentations: this._instrumentations,
+    });
   }
 
   /* Experimental getter method: not currently part of the upstream
